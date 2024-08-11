716b5782f022127075998670773edda2901e3ed6
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2024-08-11 23:35:03 +0530

    Commit

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -6,11 +6,12 @@ import { Component } from '@angular/core';
   imports: [],
   // templateUrl: './notification.component.html',
   // template : '<div><p>This app uses cookies for better user experience!!</p></div>',
-  template : `<div>
+  template : `<div class = "notification-div">
                   <p>This app uses cookies for better user experience!!</p>
                 </div>`,
 
-  styleUrl: './notification.component.css'
+  // styleUrl: './notification.component.css'
+  styles: [".notification-div{margin: 10px 0px; padding: 10px 20px; background-color: #FAD7A0; text-align: center}", "p{font-size: 14px;}" ]
 })
 export class NotificationComponent {
 
